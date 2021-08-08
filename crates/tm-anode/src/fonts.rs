86254489fb89b2b545828b222d25236d4031af24
Layl Bongers <2385329-layl@users.noreply.gitlab.com>
2021-08-08 14:37:04 +0200

    Use font-kit instead of dwrote

--- crates/tm-anode/src/fonts.rs
+++ crates/tm-anode/src/fonts.rs
@@ -1,6 +1,8 @@
 use std::ffi::CString;
 
-use dwrote::{FontCollection, FontDescriptor, FontStretch, FontStyle, FontWeight};
+use font_kit::{
+    family_name::FamilyName, handle::Handle, properties::Properties, source::SystemSource,
+};
 use machinery::{identifier, Identifier, RegistryStorage};
 use machinery_api::{
     foundation::ApiRegistryApi,
@@ -11,17 +13,23 @@ pub fn register(registry: &ApiRegistryApi, registry_storage: &mut RegistryStorag
     unsafe {
         let range = TtfRangeT { start: 32, n: 95 };
 
-        // Lookup the font using DirectWrite
-        let font_collection = FontCollection::system();
-        let desc = FontDescriptor {
-            family_name: "Consolas".to_string(),
-            weight: FontWeight::Regular,
-            stretch: FontStretch::Normal,
-            style: FontStyle::Normal,
+        // Lookup the font using font-kit
+        let source = SystemSource::new();
+        let font = source
+            .select_best_match(
+                &[
+                    FamilyName::Title("Consolas".to_string()),
+                    FamilyName::Monospace,
+                ],
+                &Properties::new(),
+            )
+            .unwrap();
+        let font_path = if let Handle::Path { path, .. } = font {
+            path
+        } else {
+            panic!("Unable to locate any font.");
         };
-        let font = font_collection.get_font_from_descriptor(&desc).unwrap();
-        let font_file = &font.create_font_face().get_files()[0];
-        let font_path = font_file.get_font_file_path().unwrap();
+
         let font_path_c = CString::new(font_path.to_str().unwrap()).unwrap();
 
         let descriptor = FontDescriptorT {
